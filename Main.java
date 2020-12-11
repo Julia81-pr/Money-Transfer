41a9edfe26a8c4fcba67f8121b99eed32db6bc25
Julia81-pr <75021260+Julia81-pr@users.noreply.github.com>
2020-12-11 21:34:42 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,1 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+      //  int transferAmount = ;
+        int finalBalance =  currentBalance -500_000_000;
 
+        System.out.println("Client balance: "+ finalBalance);
+        System.out.println("Client balance: "+ finalBalance);
+        System.out.println("Client balance: "+ finalBalance);
+        System.out.println("Client balance: "+ finalBalance);
+        System.out.println("Client balance: "+ finalBalance);
+        System.out.println("Client balance: "+ finalBalance);
+    }
+}
